75deded0bb784339246a51a4f2f19564c1100d6a
chitrakakkar <India@123>
2016-04-28 16:55:49 -0500

    Designed the Gui

--- src/com/Chitra/CoffeeGuiForm.java
+++ src/com/Chitra/CoffeeGuiForm.java
@@ -1,7 +1,42 @@
 package com.Chitra;
 
+import javax.swing.*;
+
 /**
  * Created by chitrakakkar on 4/23/16.
  */
-public class CoffeeGuiForm {
+public class CoffeeGuiForm
+{
+    private JScrollPane scrollPane1;
+    private JCheckBox smallCheckBox;
+    private JCheckBox mediumCheckBox;
+    private JCheckBox largeCheckBox;
+    private JTextField SmallQuantityTextBox;
+    private JTextField MediumSizeTextBox;
+    private JTextField LargeSizeTextBox;
+    private JCheckBox cookiesCheckBox;
+    private JCheckBox cupCakesCheckBox;
+    private JCheckBox iceCreamCheckBox;
+    private JCheckBox candyCheckBox;
+    private JCheckBox chipsCheckBox;
+    private JTextField SumTextField;
+    private JTextField VATTextField;
+    private JTextField TotalSumText;
+    private JList list1;
+    private JButton writeINToReportTableButton;
+    private JPasswordField AdminPassword;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton addButton;
+    private JButton updateButton;
+    private JButton deleteButton;
+    private JTextField DateText;
+    private JTextField textField1;
+    private JButton extractDataButton;
+    private JTextArea textArea1;
+    private JButton writeToaFileButton;
+    private JTable table1;
+    private JLabel TagLabel;
+
+
 }
